dcda6902575bff03fd8be16b62724a1cd8291a52
misspran <vipran@gmail.com>
2014-10-01 13:16:17 -0500

    Initial Commit

---
+++ mySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mySafari
+//
+//  Created by Vi on 10/1/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mySafari
+//
+//  Created by Vi on 10/1/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
